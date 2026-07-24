58e8b213f8819772e6913fce0ef8df8792e5fc09
deepuu-git <deepusah7907@gmail.com>
2026-07-24 20:36:12 +0530

    08: trpc setup

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,7 +2,7 @@ import { auth } from "@/lib/auth";
 import { HomeView } from "@/modules/home/ui/views/home-view";
 import { redirect } from "next/navigation";
 import { headers } from "next/headers";
-
+import { caller } from "@/trpc/server";
 const Page = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
@@ -11,6 +11,8 @@ const Page = async () => {
   if (!session) {
     redirect("/sign-in");
   }
+
+  
 
 
   return <HomeView />
