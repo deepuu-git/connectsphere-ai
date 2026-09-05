b14e328b6cabee299afff45ec37a4638b2471d77
deepuu-git <deepusah7907@gmail.com>
2026-09-05 19:10:29 +0530

    21: maating variants

--- src/components/empty-state.tsx
+++ src/components/empty-state.tsx
@@ -3,18 +3,20 @@ import  Image  from "next/image";
 interface Props {
     title: string;
     description: string;
+    image?: string;
 
 };
 
 export const EmptyState = ({
     title,
     description,
+    image = "/empty.svg"
 
 }: Props) => {
     return (
         <div className="flex flex-col items-center justify-center">
                 <Image 
-                src="/empty.svg" 
+                src={image}
                 alt="Empty" 
                 width={240} 
                 height={240} 
